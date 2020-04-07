a09bcf70d81fc6b5e30fa0eab823e73b801ae476
Brandon Bednar <bwbednar@ksu.edu>
2020-04-07 18:16:55 -0500

    Q3 work done

--- PointOfSale/CustomizeCobblerControl.xaml.cs
+++ PointOfSale/CustomizeCobblerControl.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExamTwoCodeQuestions.Data;
 
 namespace ExamTwoQuestions.PointOfSale
 {
@@ -21,6 +22,44 @@ namespace ExamTwoQuestions.PointOfSale
         public CustomizeCobblerControl()
         {
             InitializeComponent();
+            
         }
+
+        /// <summary>
+        /// Private event for creating the text for the Summary Text Box.
+        /// Shows that changes have been made to the cobbler.
+        /// </summary>
+        /// <param name="sender">The control that was changed</param>
+        /// <param name="e">Routed Event Args</param>
+        private void ChangeSelectionTextBox(object sender, RoutedEventArgs e)
+        {
+            var cobbler = DataContext as Cobbler;
+            StringBuilder summary = new StringBuilder();
+            switch (cobbler.Fruit)
+            {
+                case FruitFilling.Peach:
+                    summary.Append("Peach Cobbler ");
+                    break;
+                case FruitFilling.Cherry:
+                    summary.Append("Cherry Cobbler ");
+                    break;
+                case FruitFilling.Blueberry:
+                    summary.Append("Blueberry Cobbler ");
+                    break;
+                default:
+                    summary.Append("Error ");
+                    break;
+            }
+            
+            if (cobbler.WithIceCream)
+            {
+                summary.Append("With Ice Cream");
+            }
+            else summary.Append("Without Ice Cream");
+
+            tbSelection.Text = summary.ToString();
+        }
+
+       
     }
 }
